e6beba1dabb914cf5ee609919d48a2a1c09c1e8e
MdForhadHossainBabu <developerforhad2@gmail.com>
2024-06-14 04:09:42 +0600

    added to work page section

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,4 +1,29 @@
-const Work = () =>{
+"use client"
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import {Swiper, SwiperSlide} from 'swiper/react'
+import "swiper/css";
+import{BsArrowUpRight, BsGithub} from "react-icons/bs"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: '01',
+    category: 'frond-end',
+    title: 'project 1',
+    description:
+   'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis iusto minima dicta fugit cum.',
+  stack: [{ name: "Html 5", }, { name: "Css 3", }, { name: "Javascript", }],
+  image: "",
+  live: "",
+  github:""
+  },
+];
+
+const Work = () => {
  return (
   <div>this is work</div>
  )
